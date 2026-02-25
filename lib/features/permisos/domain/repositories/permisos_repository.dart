3f616c0c3aab967a8dcacb8d5587e234f3ec2635
Oscar Herrera <osedhelu@gmail.com>
2026-02-24 22:52:56 -0500

    refactor(permisos, turno): remove unused permissions and turno feature files

--- lib/features/permisos/domain/repositories/permisos_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:wap_xcontrol/features/permisos/domain/entities/permisos.dart';
-
-abstract class PermisosRepository {
-  Future<Permisos> getById(int id);
-  Future<List<Permisos>> getAll();
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xtdespachos/main.dart';
+import 'package:wap_xcontrol/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
